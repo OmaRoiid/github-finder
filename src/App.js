8bc894f5b5d0f686cd91ba9e2aef04490a139706
Omar Salem <omarsalem977@gmail.com>
2023-03-17 15:17:17 +0200

    add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Footer } from "./components/layout/Footer";
 import { Navbar } from "./components/layouts/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Notfound } from "./pages/NotFound";
+
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>C</main>
+        <main className="container mx-auton px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<Notfound />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+    </div>
+  );
+};
